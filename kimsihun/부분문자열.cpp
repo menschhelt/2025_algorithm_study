2d721d31fb78c583a3e81a45f9cb529e6c2df756
Chris <menschhelt329@gmail.com>
2025-01-18 01:02:24 +0900

    Create 부분문자열.cpp
    [PGS] 부분 문자열 / Lv. 0 / 5분

---
+++ kimsihun/부분문자열.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string str1, string str2) {
+    int answer;
+    for (int i =0; i<str2.size() - str1.size() +1; i++){
+        answer = 1;
+        for (int j =0; j<str1.size(); j++){
+            if(str2[i+j] != str1[j]){
+                answer = 0;
+                break;
+            }
+        }
+        if (answer == 1){
+            return answer;
+        }
+    }
+    return 0;
+}
