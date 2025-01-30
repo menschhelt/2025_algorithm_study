7ede0618b83935ee3fadae0ed32921ce7b594e82
Chris <menschhelt329@gmail.com>
2025-01-30 23:30:34 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.26 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string my_string) {
+    vector<string> answer;
+    string temp = "";
+    for (int i =0; i<my_string.size() ; i++){
+        if (my_string[i] == ' '){
+            answer.push_back(temp);
+            temp = "";
+            continue;
+        }
+        temp += my_string[i];
+    }
+    answer.push_back(temp);
+    return answer;
+}
